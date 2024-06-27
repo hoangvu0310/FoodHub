f20a547a6d567c91f4063d1ccec75b9cb2365212
hoangvu0310 <hoangvu031003@gmail.com>
2024-06-27 16:53:00 +0700

    first commit
    create project, import some fonts, image and config splash screen

---
+++ app/src/main/java/com/example/foodhub/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.foodhub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        installSplashScreen()
+        Thread.sleep(1000)
+        setContent {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/foodhub/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.foodhub.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val circleBeige = Color(0xFFFFECE7)
+val appOrange = Color(0xFFFE724C)
+val welcomeDescriptionText = Color(0xFF30384F)
+val lightSofiaPro = Color(0xFF8C8A9D)
+val regularSofiaPro = Color(0xFF9796A1)
+val semiBoldSofiaPro = Color(0xFF323643)
